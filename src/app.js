2adede0a163065f473dc9a3771555f7e5695c65a
Pablo Chorda <pablochorda365@gmail.com>
2024-09-25 19:34:03 +0200

    Realizamos un servidor web donde alojamos una red social tipo twitter

---
+++ src/app.js
@@ -0,0 +1,20 @@
+const { envs } = require('./config/env')
+const { startServer } = require('./server/server')
+
+
+const main = () => {
+    startServer({
+        port: envs.PORT,
+        public_path: envs.PUBLIC_PATH
+    })
+}
+
+
+
+// Función agnóstica autoconvocada
+// Agnóstica porque no tien nombre
+// Autoconvocada porque la ejecutamos con los parentesis
+
+( async () => {
+    main()
+})()
